d2978acbbd6620a40adfc25a1167dbd788963695
IskandarRafikov <isko.85@mail.ru>
2022-08-23 17:15:16 +0300

    hw-01 Новые activity - описание выбранного фильма и подделиться с другом

---
+++ app/src/main/java/com/otus/homework/pickmovie/ShareToFriendActivity.kt
@@ -0,0 +1,11 @@
+package com.otus.homework.pickmovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShareToFriendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share_to_friend)
+    }
+}
