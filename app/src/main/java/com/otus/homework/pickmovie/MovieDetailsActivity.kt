0c23a328eb5be1992ed043b26fa173fb1b7a539e
IskandarRafikov <isko.85@mail.ru>
2022-09-02 09:08:26 +0300

    hw-01 Добавлен контейнер-список для хранения информации о фильмах
    # Conflicts:
    #	.gradle/7.3.3/executionHistory/executionHistory.bin
    #	.gradle/7.3.3/executionHistory/executionHistory.lock
    #	.gradle/7.3.3/fileHashes/fileHashes.bin
    #	.gradle/7.3.3/fileHashes/fileHashes.lock
    #	.gradle/7.3.3/fileHashes/resourceHashesCache.bin
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.gradle/file-system.probe
    #	.idea/misc.xml

--- app/src/main/java/com/otus/homework/pickmovie/MovieDetailsActivity.kt
+++ app/src/main/java/com/otus/homework/pickmovie/MovieDetailsActivity.kt
@@ -1,15 +1,25 @@
 package com.otus.homework.pickmovie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MovieDetailsActivity : AppCompatActivity() {
+    companion object {
+        const val HAS_COMMENT = "false"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
 
         findViewById<Button>(R.id.buttonOk).setOnClickListener {
+            val comment = findViewById<TextView>(R.id.commentView)
+            val hasText = if(comment.length() > 0) "true" else "false"
+
+            setResult(RESULT_OK, Intent().putExtra(HAS_COMMENT, hasText))
             finish()
         }
     }
